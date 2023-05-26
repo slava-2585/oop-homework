a02205f3c4be6946158008b4402dd607b0abb7c6
Nikiforov Vyacheslav <slava-2585@yandex.ru>
2023-05-26 17:50:18 +0300

    Initial commit task1 and task2

---
+++ main.py
@@ -0,0 +1,60 @@
+#https://github.com/netology-code/py-homeworks-basic/blob/new_oop/6.classes/README.md
+
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+ 
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)  
+        
+    def rate_lect(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in Lecturer.courses_attached and course in self.courses_in_progress and grade < 11:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка' 
+ 
+     
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        
+        
+class Lecturer (Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+        
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+ 
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+ 
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+ 
+# print(best_student.grades)
